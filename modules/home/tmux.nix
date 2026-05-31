17671eb2204a680e59bc6ecdeec7590f42148129
galleb <s@omfm.ru>
2026-05-31 19:54:17 +0300

    tmux icon

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -29,7 +29,7 @@
       set -g status-left-length 100
 
       set -g status-bg black
-      set -g status-left "#[fg=yellow,bg=black]#[fg=default,bg=yellow]   #S #[fg=blue]#[fg=black,bg=blue] 󰰦 󰰑 󰰩 󰰲 #[fg=blue,bg=default] "
+      set -g status-left "#[fg=yellow,bg=black]#[fg=default,bg=yellow]   #S #[fg=blue]#[fg=black,bg=blue] 󰰦 󰰑 󰰩 󰰲 #[fg=blue,bg=default] "
       set -g status-right '#[fg=color8]#[fg=black,bg=color8] #W #[fg=color8,bg=blue]#[fg=blue,bg=blue]#[fg=black,bg=blue] %H:%M #[fg=blue,bg=yellow]#[fg=default,bg=yellow] 󰥳 #[fg=yellow,bg=black]'
       setw -g window-status-format '#[fg=yellow]#[fg=black,bg=yellow]#(omvoid-tmux-icons-helper #I) #[fg=yellow,bg=default] #[fg=red,bg=default]#W'
       setw -g window-status-current-format '#[fg=yellow]#[fg=black,bg=yellow]#(omvoid-tmux-icons-helper #I) #[fg=black,bg=yellow] #W #[fg=yellow,bg=default]'
