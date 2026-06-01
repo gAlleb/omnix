ecfc8dcfb72ac960d9d9f8623c04867bc4359f63
galleb <s@omfm.ru>
2026-06-01 19:22:44 +0300

    feat: yggdrasil client + restrictive firewall rules
    - enable services.yggdrasil with pinned IfName=ygg0 and ephemeral keys
      (new address every boot, fine for client-only use)
    - public peers: averyan.ru tcp+tls
    - firewall: trust docker0, drop everything from ygg0,
      allow LAN 192.168.1.0/24 and SSH from non-ygg interfaces

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,7 +1,29 @@
 { config, lib, pkgs, ... }:
 {
   networking.networkmanager.enable = true;
-  networking.firewall.enable = true;
-
   programs.nm-applet.enable = true;
+
+  services.yggdrasil = {
+    enable = true;
+    persistentKeys = false;
+    settings = {
+      IfName = "ygg0";
+      Peers = [
+        "tcp://ygg-msk-1.averyan.ru:8363"
+        "tls://ygg-msk-1.averyan.ru:8362"
+      ];
+    };
+  };
+
+  networking.firewall = {
+    enable = true;
+
+    trustedInterfaces = [ "docker0" ];
+
+    extraInputRules = ''
+      iifname "ygg0" drop
+      ip saddr 192.168.1.0/24 accept
+      tcp dport 22 accept
+    '';
+  };
 }
