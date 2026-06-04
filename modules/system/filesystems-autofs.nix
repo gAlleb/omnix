e17ad2abb0da8ff27b86fc7d9b63a9d957f4838a
galleb <s@omfm.ru>
2026-06-04 09:05:49 +0300

    fix autofs rclone

--- modules/system/filesystems-autofs.nix
+++ modules/system/filesystems-autofs.nix
@@ -1,9 +1,14 @@
 { config, lib, pkgs, ... }:
+let
+  # Создаем симлинк rclonefs в системном профиле
+  rclonefs-helper = pkgs.runCommand "rclonefs-helper" {} ''
+    mkdir -p $out/bin
+    ln -s ${pkgs.rclone}/bin/rclone $out/bin/rclonefs
+    ln -s ${pkgs.rclone}/bin/rclone $out/bin/mount.rclonefs
+    ln -s ${pkgs.rclone}/bin/rclone $out/bin/mount.rclone
+  '';
+in
 {
-  # Equivalent of the old config/rclone.sh — autofs + rclone + cifs/nfs/ntfs.
-  # We don't describe individual mounts in Nix; the mapping changes too often,
-  # so /etc/autofs/auto.mymounts is shipped as an editable template (below).
-
   services.autofs = {
     enable = true;
     autoMaster = ''
@@ -11,45 +16,41 @@
     '';
   };
 
+  # Добавляем наш хелпер в окружение autofs
+  systemd.services.autofs.path = [
+    pkgs.rclone
+    rclonefs-helper
+    pkgs.fuse3
+  ];
+
   programs.fuse.userAllowOther = true;
 
   environment.systemPackages = with pkgs; [
     rclone
+    rclonefs-helper # <-- Это создаст стабильный путь /run/current-system/sw/bin/rclonefs
     cifs-utils
     nfs-utils
     ntfs3g
     exfatprogs
   ];
 
-  # /etc/autofs/auto.mymounts is shipped as an editable template:
-  # systemd-tmpfiles `C` rule copies it once from the nix-store template
-  # on first rebuild, then leaves it alone — your edits survive
-  # subsequent `nixos-rebuild`.  Remove the file if you want the
-  # template re-deployed.
-  #
-  # Also create the rclone-as-mount-helper symlinks autofs expects
-  # (/sbin/mount.rclone, /usr/bin/rclonefs).
   systemd.tmpfiles.rules = [
     "C /etc/autofs/auto.mymounts 0644 root root - ${pkgs.writeText "auto.mymounts-template" ''
-      #
-      # fstype=fuse.rclonefs works, fstype=rclone does not.
-      # The rclone -> /sbin/mount.rclone symlink is created by systemd-tmpfiles below.
-      #
       # NTFS:
-      #   win11-data -fstype=ntfs-3g,uid=1000,gid=1000,windows_names :/dev/nvme0n1p4
-      #
+      win11-data -fstype=ntfs-3g,uid=1000,gid=1000,windows_names :/dev/nvme0n1p4
+
       # CIFS:
-      #   music -fstype=cifs,rw,credentials=/etc/autofs/credentials,noperm ://192.168.1.14/music
-      #
+      music -fstype=cifs,rw,credentials=/etc/autofs/credentials,noperm ://192.168.1.14/music
+
       # NFS:
-      #   truenas2 -rw,soft 192.168.1.14:/mnt/my-1tb-pool/music
-      #
+      truenas2 -rw,soft 192.168.1.14:/mnt/my-1tb-pool/music
+
       # CLOUD:
-      #   GoogleDrive -allow_other,args2env,fstype=fuse.rclonefs,config=/root/.config/rclone/rclone.conf,cache-db-purge,allow-other,vfs-cache-mode=writes :GoogleDrive:
-      #   YandexDisk  -allow_other,args2env,fstype=fuse.rclonefs,config=/root/.config/rclone/rclone.conf,cache-db-purge,allow-other,vfs-cache-mode=writes :YandexDiskLinux:
+      # Передаем абсолютный путь к бинарнику прямо внутрь опции fstype!
+      # Так mount.fuse запустит rclonefs напрямую, проигнорировав пустой PATH в bash.
+      # GoogleDrive -allow_other,args2env,fstype=fuse./run/current-system/sw/bin/rclonefs,config=/root/.config/rclone/rclone.conf,cache-db-purge,allow-other,vfs-cache-mode=writes :GoogleDrive:
+      # YandexDisk  -allow_other,args2env,fstype=fuse./run/current-system/sw/bin/rclonefs,config=/root/.config/rclone/rclone.conf,cache-db-purge,allow-other,vfs-cache-mode=writes :YandexDiskLinux:
     ''}"
-    "L+ /sbin/mount.rclone - - - - ${pkgs.rclone}/bin/rclone"
-    "L+ /usr/bin/rclonefs   - - - - ${pkgs.rclone}/bin/rclone"
   ];
 
   services.rpcbind.enable = true;
